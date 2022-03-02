e98ed06afee257f380db2165e1b548d82996265c
Aleandro2119 <aleandro.basilico@gmail.com>
2022-03-02 15:28:02 +0100

    Add bootstrap and style

--- opp/index.php
+++ opp/index.php
@@ -33,24 +33,36 @@ $movie3 = new Movie('Il Signore degli Anelli - Il ritorno del re', 'Peter Jackso
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>PHP-OOP</title>
 </head>
 
 <body>
-    <h2>Il titolo è: <?= $movie1->title ?></h2>
-    <p>Diretto da: <?= $movie1->author ?></p>
-    <p>Uscito nei cinema nel: <?= $movie1->year ?></p>
-    <p>Voto: <?= $movie1->vote ?></p>
+    <header>
+        <h1 class="text-center text-white p-3">FILM</h1>
+    </header>
 
-    <h2>Il titolo è: <?= $movie2->title ?></h2>
-    <p>Diretto da: <?= $movie2->author ?></p>
-    <p>Uscito nei cinema nel: <?= $movie2->year ?></p>
-    <p>Voto: <?= $movie2->vote ?></p>
+    <main>
+        <div class="container">
+            <div class="text-center m-5">
+                <h2>Titolo: <?= $movie1->title ?></h2>
+                <p>Diretto da: <?= $movie1->author ?></p>
+                <p>Uscito nei cinema nel: <?= $movie1->year ?></p>
+                <p>Voto: <?= $movie1->vote ?></p>
 
-    <h2>Il titolo è: <?= $movie3->title ?></h2>
-    <p>Diretto da: <?= $movie3->author ?></p>
-    <p>Uscito nei cinema nel: <?= $movie3->year ?></p>
-    <p>Voto: <?= $movie3->vote ?></p>
+                <h2>Titolo: <?= $movie2->title ?></h2>
+                <p>Diretto da: <?= $movie2->author ?></p>
+                <p>Uscito nei cinema nel: <?= $movie2->year ?></p>
+                <p>Voto: <?= $movie2->vote ?></p>
+
+                <h2>Titolo: <?= $movie3->title ?></h2>
+                <p>Diretto da: <?= $movie3->author ?></p>
+                <p>Uscito nei cinema nel: <?= $movie3->year ?></p>
+                <p>Voto: <?= $movie3->vote ?></p>
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
